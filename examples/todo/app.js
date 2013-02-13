ff2e57b9c059cedebbb4792cfcb54fa18b3fdbc7
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-13 14:57:44 -0500

    Todo example: Added code to allow basic pub/sub

--- examples/todo/app.js
+++ examples/todo/app.js
@@ -8,4 +8,9 @@ angular.module('TodoExample',
     $routeProvider.when('/add-todo', {});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
-  }]);
+  }])
+	.run(function($rootScope) {
+		$rootScope.$on('handleEmit', function(event, args) {
+			$rootScope.$broadcast('handleBroadcast', args);
+		});
+	});  
