154ca632cb4ad610ad7bec0275068ac9644f4996
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-13 14:00:38 -0500

    Todo example: Started to add navigable routes

---
+++ examples/todo/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('TodoExample', 
+	['Centralway.lungo-angular-bridge']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/add-todo', {});
+    $routeProvider.otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
+  }]);
