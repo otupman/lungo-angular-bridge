4c0e35f24003f0a18309a836fd3f7b213fc7f84d
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-12 21:11:02 -0500

    Added hard-refresh button to get around the fact that refreshing the page won't work as the path will have changed

--- examples/js/controllers.js
+++ examples/js/controllers.js
@@ -2,6 +2,12 @@
 
 function AppCtrl($scope, $location) {
     $scope.name = "Some name";
+    /**
+	  * A short cut to manually refresh the application from the example/ directory
+	  */
+    $scope.refreshApplication = function() {
+    	window.location = '/examples/index.html';
+    }
     AppRouter.instance = AppRouter(Lungo, $location, $scope);
 }
 
