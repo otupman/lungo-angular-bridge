1c6a5bf91b4f98d8a4ac517a622cf0e47dfe9513
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-15 15:57:42 -0500

    Trying to test the index for issue 23

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -7,7 +7,24 @@ describe('Angular Lungo Bridge', function() {
 		expect(browser().location().url()).toBe('/');
 	});
 
+	describe('Showing dynamic cw-view loaded templates', function() {
 
+		beforeEach(function() {
+			browser().navigateTo('../../examples/simple/index.html');
+			element('a[href="dynamic"]').click();
+		});
+
+		it('should display the dynamic page', function() {
+			expect(element('section[id="dynamic"]').count()).toEqual(1);
+		});
+
+		it('should call section once', function() {
+			pause();
+			expect(element('#lungo-router-section').text()).toEqual(1); 
+		});
+
+	});
+   
 	describe('Sample Test', function() {
 
 		beforeEach(function() {
