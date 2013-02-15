6a903a8f47344a8e7275b69caf4dffc249da3a5f
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-15 10:44:01 -0500

    Playing around with using directives to declaratively enable lungo initialisation in directives using data-* attributes

--- examples/simple/js/directives.js
+++ examples/simple/js/directives.js
@@ -8,10 +8,41 @@ angular.module('BridgeExample.directives', []).
       elm.text(version);
     };
   }])
+  .directive('wrapLungo', function() {
+  	return {
+  		restrict: 'A'
+  		, priority: 0
+  		, transclude: true
+  		, link: function(scope, iElement, iAttrs, controller) {
+  			console.log(iElement);
+  		}
+  	}
+  })
+  .directive('compileLungo', function() {
+  	return {
+  		restrict: 'A'
+  		, compile: function compile(tElement, tAttrs, transclude) {
+  			console.log(transclude);
+	      return {
+	        pre: function preLink(scope, iElement, iAttrs, controller) { 
+	        	console.log('compile prelink: ');
+	        	console.log(iElement);
+	        },
+	        post: function postLink(scope, iElement, iAttrs, controller) {
+	        	console.log('compile postlink: ');
+	        	console.log(iElement);
+	        	Lungo.Boot.Data.init(iElement[0]);
+	        }
+	      }
+	    }
+  	}
+  })
   .directive('directiveAndLungoTest', function() {
   	return {
-            restrict: 'E',
-            template: '<a data-icon="home">Has home icon?</a>'
-
+            restrict: 'E'
+            , template: '<a wrap-lungo compile-lungo data-icon="home">Has home icon?</a>'
+            , link: function(scope, iElement, iAttrs, controller) {
+            	console.log(iElement);
+            }
         }
   });
