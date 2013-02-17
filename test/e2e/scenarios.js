4077baf1dfafd237e5fa099d5edca1921701c353
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2013-02-16 19:14:23 -0600

    Added sample Simple E2E Tests. Fixes #15

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Angular Lungo Bridge', function() {
+describe('Angular Lungo Bridge Simple App E2E Tests', function() {
 
 	it('Should redirect index.html to index.html', function() {
 		browser().navigateTo('../../examples/simple/index.html');
@@ -18,16 +18,29 @@ describe('Angular Lungo Bridge', function() {
 			expect(element('cw-view').count()).toEqual(1);
 		});
 
-		it('Navigate to a different static section through the UI', function() {
+		it('Should render a static section using the UI', function() {
 			element('a[href="section1"]').click();
 			expect(browser().location().url()).toBe('/section1');
 		});
 
-		it('Navigate to a different dynamic section through the UI', function() {
+		it('Should render a dynamic section through the UI', function() {
 			element('a[href="dynamic"]').click();
 			expect(browser().location().url()).toBe('/dynamic');
 			expect(repeater('ul.items li').count()).toBe(3);
 		});
+
+		it('Should render the page previously loaded', function() {
+			element('a[href="dynamic"]').click();
+			expect(browser().location().url()).toBe('/dynamic');
+			element('a[href="back"]').click();
+			expect(element('header').attr('data-title')).toBe("Simple Application");
+		});
+
+		it('Should render person specific id in the view', function() {
+			element('a[href="person/view/2"]').click();
+			expect(browser().location().url()).toBe('/person/view/2');
+			expect(element('span.id').text()).toBe("2");
+		})
 	});
 
 });
