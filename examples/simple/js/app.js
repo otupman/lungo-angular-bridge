155a3a4e9e401efc3b84b0baa89d3509ae779a47
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2013-02-16 18:03:04 -0600

    Added more routing e2e tests. Wrapped vars in app.js with single quotes, otherwise Angular will try to load them as javascript variables

--- examples/simple/js/app.js
+++ examples/simple/js/app.js
@@ -9,7 +9,7 @@ angular.module('BridgeExample',
     // First section is embedded in index.html, so no parameters
     $routeProvider.when('/section1', {});
     // Dynamic section is loaded via cw-view
-    $routeProvider.when('/dynamic', { templateUrl: 'examples/simple/partials/dynamic.html', controller: DynamicCtrl});
+    $routeProvider.when('/dynamic', { templateUrl: 'examples/simple/partials/dynamic.html', controller: 'DynamicCtrl'});
     // Demo of using query parameters for search terms
     $routeProvider.when('/search', { templateUrl: 'examples/simple/partials/search.html', controller: 'SearchCtrl'});
     // Demo of using IDs within URLs - note the section ID then article ID and **then** the person ID
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -8,16 +8,25 @@ describe('Angular Lungo Bridge', function() {
 	});
 
 
-	describe('Sample Test', function() {
+	describe('Testing routing through angular', function() {
 
 		beforeEach(function() {
 			browser().navigateTo('../../examples/simple/index.html');
 		});
 
-
 		it('Should find the cw-view through the browser', function() {
 			expect(element('cw-view').count()).toEqual(1);
 		});
+
+		it('Navigate to a different static section through the UI', function() {
+			element('a[href="section1"]').click();
+			expect(browser().location().url()).toBe('/section1');
+		});
+
+		it('Navigate to a different dynamic section through the UI', function() {
+			element('a[href="dynamic"]').click();
+			expect(browser().location().url()).toBe('/dynamic');
+		});		
 	});
 
 });
