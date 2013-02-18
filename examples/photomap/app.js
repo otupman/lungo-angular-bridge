4b724b815ab611616d3c9e3a1b68c92102a86198
Martin Naumann <martin.naumann@centralway.com>
2013-02-18 20:32:21 +0100

    Removed hard-coding of ng-controller in views.

--- examples/photomap/app.js
+++ examples/photomap/app.js
@@ -5,8 +5,8 @@
 angular.module('photoMapApp', 
 	['Centralway.lungo-angular-bridge']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/camera', {templateUrl: "partials/camera.html"});
-    $routeProvider.when('/map', {templateUrl: "partials/map.html"});
+    $routeProvider.when('/camera', {templateUrl: "partials/camera.html", controller: "PhotoCtrl"});
+    $routeProvider.when('/map', {templateUrl: "partials/map.html", controller: "MapCtrl"});
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(false);
   }]);  
