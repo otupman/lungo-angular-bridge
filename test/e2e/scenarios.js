3e1c0410608d7b2946f2294eb83e1000b9e347e0
Martin Naumann <martin.naumann@centralway.com>
2013-02-19 14:02:47 +0100

    Adding a test to verify Issue #19 and adjusting scenario according to change in link target from 'back' to '/'

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -32,7 +32,7 @@ describe('Angular Lungo Bridge Simple App E2E Tests', function() {
 		it('Should render the page previously loaded', function() {
 			element('a[href="dynamic"]').click();
 			expect(browser().location().url()).toBe('/dynamic');
-			element('a[href="back"]').click();
+			element('a[href="/"]').click();
 			expect(element('header').attr('data-title')).toBe("Simple Application");
 		});
 
@@ -42,5 +42,23 @@ describe('Angular Lungo Bridge Simple App E2E Tests', function() {
 			expect(element('span.id').text()).toBe("2");
 		})
 	});
+    
+    describe('Testing rendering of data-* attributes of directives', function() {
+        beforeEach(function() {
+            browser().navigateTo('../../examples/simple/index.html');
+        });
+        
+        it('should have a directive-and-lungo-test element present', function() {
+            expect(element("directive-and-lungo-test").count()).toBe(1);
+        });
+        
+        it('should have the data-icon set to "home"', function() {
+            expect(element('directive-and-lungo-test a').attr('data-icon')).toBe('home');
+        });
+        
+        it('should have the data-icon rendered to the DOM', function() {
+            expect(element('directive-and-lungo-test a span.icon').attr('class')).toBe('icon home');
+        });
+    });
 
 });
