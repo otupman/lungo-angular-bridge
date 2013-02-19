517481001a6d6ba988f29eec4beac495c8311040
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-19 17:36:53 -0500

    Added tests for passing of resources as part of the element's attributes. Added a expectation that not passing resources will, well, pass no resources to Lungo

--- test/spec/directives.spec.js
+++ test/spec/directives.spec.js
@@ -25,10 +25,28 @@ describe('directives', function() {
       inject(function($compile, $rootScope) {
         var element = $compile('<div lab-routing></div>')($rootScope);
         expect(Lungo.init).toHaveBeenCalled();
+        expect(Lungo.init.mostRecentCall.args[0]).toEqual({});
       });
 
       
 		});
+
+		describe('with resources', function() {
+			it('should pass on a single file correctly', function() {
+				inject(function($compile, $rootScope) {
+					var element = $compile('<div lab-routing resources="singleFile.html"></div>')($rootScope);
+					expect(Lungo.init.mostRecentCall.args[0]).toEqual({resources: 'singleFile.html'});
+				});
+			});
+
+			it('should pass on a file list correctly', function() {
+				inject(function($compile, $rootScope) {
+					var element = $compile('<div lab-routing resources="first.html,second.html"></div>')($rootScope);
+					expect(Lungo.init.mostRecentCall.args[0]).toEqual({resources: ['first.html', 'second.html']});
+				});
+			});
+		})
+
 	});
 
 });
