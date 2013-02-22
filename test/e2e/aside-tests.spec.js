82d8448e1cbf2236ca61f8a48e9239e1dcc22530
Oliver Tupman <oliver.tupman@centralway.com>
2013-02-22 17:53:38 -0500

    Initial test for asides that fails (yay?)

---
+++ test/e2e/aside-tests.spec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+describe('Aside tests', function() {
+	var ASIDE_WAIT = 1;
+
+	beforeEach(function() {
+		browser().navigateTo('../../examples/simple/index.html');
+		sleep(1);
+	});
+
+
+	describe('Aside across sections', function() {
+		it('should hide when navigating to a new section', function() {
+			element('#triggerLeftAsideLink').click();
+			sleep(ASIDE_WAIT);
+			element('#aside1_dynamic').click();
+			sleep(ASIDE_WAIT);
+
+			expect(element('#aside1:visible', 'Lefthand aside').count()).toBe(0);
+		});
+	});
+
+	describe('Basic aside operations', function() {
+		it('should initially be hidden', function() {
+			expect(element('#aside1:visible').count()).toBe(0);
+		});
+
+		it('should display the aside', function() {
+			element('#triggerLeftAsideLink').click();
+			sleep(ASIDE_WAIT);
+
+			expect(element('#aside1:visible').count()).toBe(1);
+		});
+
+		it('should hide a previously-displayed aside', function() {
+			element('#triggerLeftAsideLink').click();
+			sleep(ASIDE_WAIT);
+
+			element('#triggerLeftAsideLink').click();
+			sleep(ASIDE_WAIT);
+
+			expect(element('#aside1:visible').count()).toBe(0);
+		});
+	});
+
+
+
+});
