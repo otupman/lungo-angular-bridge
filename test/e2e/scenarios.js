ec8df09e1636da4484e54ec141a6f664d5d82af4
Oliver Tupman <oliver.tupman@centralway.com>
2013-03-08 15:49:08 -0500

    Moved the directive test case into it's own spec file. Added a test case for when the directive is on a sub-page. Added a directive call to the dynamic.html file on the example.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -42,23 +42,44 @@ describe('Angular Lungo Bridge Simple App E2E Tests', function() {
 			expect(element('span.id').text()).toBe("2");
 		})
 	});
-    
-    describe('Testing rendering of data-* attributes of directives', function() {
-        beforeEach(function() {
-            browser().navigateTo('../../examples/simple/index.html');
-        });
-        
-        it('should have a directive-and-lungo-test element present', function() {
-            expect(element("directive-and-lungo-test").count()).toBe(1);
-        });
-        
-        it('should have the data-icon set to "home"', function() {
-            expect(element('directive-and-lungo-test a').attr('data-icon')).toBe('home');
-        });
-        
-        it('should have the data-icon rendered to the DOM', function() {
-            expect(element('directive-and-lungo-test a span.icon').attr('class')).toBe('icon home');
-        });
-    });
 
+	describe('Testing rendering of data-* attributes of directives', function() {    
+		describe('on the dynamic page', function() {
+			beforeEach(function() {
+        browser().navigateTo('../../examples/simple/index.html');
+        element('a[href="dynamic"]').click();
+        wait(0.5);
+      });
+      
+      it('should have a directive-and-lungo-test element present', function() {
+          expect(element("directive-and-lungo-test").count()).toBe(1);
+      });
+      
+      it('should have the data-icon set to "home"', function() {
+          expect(element('directive-and-lungo-test a').attr('data-icon')).toBe('home');
+      });
+      
+      it('should have the data-icon rendered to the DOM', function() {
+          expect(element('directive-and-lungo-test a span.icon').attr('class')).toBe('icon home');
+      });
+		});
+
+    describe('on the first page', function() {
+      beforeEach(function() {
+          browser().navigateTo('../../examples/simple/index.html');
+      });
+      
+      it('should have a directive-and-lungo-test element present', function() {
+          expect(element("directive-and-lungo-test").count()).toBe(1);
+      });
+      
+      it('should have the data-icon set to "home"', function() {
+          expect(element('directive-and-lungo-test a').attr('data-icon')).toBe('home');
+      });
+      
+      it('should have the data-icon rendered to the DOM', function() {
+          expect(element('directive-and-lungo-test a span.icon').attr('class')).toBe('icon home');
+      });
+    });
+	})
 });
