468ddf530c8d225bad7e00718b1c508d81e8ba88
Oliver Tupman <oliver.tupman@centralway.com>
2013-04-23 17:30:22 +0200

    Making an old linter a happy linter

--- src/approuter.js
+++ src/approuter.js
@@ -12,21 +12,21 @@ var AppRouter = function(Lungo, $location, $scope) {
   $location.replace = function() {
     $location.$$replace = true;
     return $location;
-  }
+  };
 
   var _hasArticle = function(path) {
     var splitPath = angular.isArray(path) ? path : path.split('/');
     return splitPath.length > _SECTION_PATH_LENGTH;
-  }
+  };
 
   var _assertElementExists = function(id) {
-    if(id.indexOf('#') == -1) {
+    if(id.indexOf('#') === -1) {
       id = '#' + id;
     }
-    if(Lungo.dom(id).length == 0) {
+    if(Lungo.dom(id).length === 0) {
       throw new Error('No such element with ID [' + id + ']');
     }
-  }
+  };
 
   var showSection = function(path) {
     var pathParts = path.split('/');
@@ -42,13 +42,13 @@ var AppRouter = function(Lungo, $location, $scope) {
   };
 
   var _isSameSection = function(path) {
-    if(routingHistory.length == 0) {
+    if(routingHistory.length === 0) {
       return false;
     }
     var currentPathParts = routingHistory[routingHistory.length-1].split('/');
     var pathParts = path.split('/');
     return currentPathParts[_SECTION_INDEX] === pathParts[_SECTION_INDEX];
-  }
+  };
 
   var _resetAsideStates = function() {
     var openAsides = Lungo.dom('aside[class*="show"]');
@@ -56,15 +56,16 @@ var AppRouter = function(Lungo, $location, $scope) {
       Lungo.View.Aside.toggle('#' + Lungo.dom(value).attr('id'));
     });
     Lungo.dom('section[class*="aside"]').removeClass('aside');
-  }
+  };
 
   var _isBack = function($location) {
     return routingHistory.length > 0 && routingHistory[routingHistory.length-2] == $location.path() && !_hasArticle($location.path());
-  }
+  };
 
   var isBack = function() {
     return _isBack($location);
-  }
+  };
+  
   $scope.$on('$routeChangeSuccess', function(next, last) {
     _resetAsideStates();
     if(_isBack($location)) {
@@ -90,21 +91,21 @@ var AppRouter = function(Lungo, $location, $scope) {
       throw new Error('No back to go back to!');
     }
     return routingHistory[routingHistory.length - 2];
-  }
+  };
 
   var back = function() {
 
-    if(routingHistory.length == 0) {
+    if(routingHistory.length === 0) {
       return;
     }
     var previousLocation = getPrevious();
     $location.path(previousLocation);
-  }
+  };
 
   return {
     back: back
     , isBack: isBack
     , isSameSection: _isSameSection
-  }
+  };
 
 };
