8ef3835a27fa12200dc5edd911c633a89279b717
Oliver Tupman <oliver.tupman@centralway.com>
2013-04-24 14:18:02 +0200

    chore() goodbye horrible mocked object, hello Jasmine spies!

--- test/spec/directives.spec.js
+++ test/spec/directives.spec.js
@@ -1,66 +1,5 @@
 'use strict';
 
-Lungo.mock = function() {
-  var _oldLungo = Lungo;
-  console.log('whee');
-  var _absoluteHistory = [];
-  //TODO: Pretty sure this could be done via a spy rather than a custom mock
-  var section = function(sectionId) {
-    _absoluteHistory.push(sectionId);
-  };
-
-  var article = function(sectionId, articleId) {
-    _absoluteHistory.push(sectionId + '/' + articleId);
-  }
-
-  var back = function() {
-    _absoluteHistory.pop();
-  }
-
-  var getHistory = function() {
-    return _absoluteHistory;
-  }
-
-  var clearHistory = function() {
-    _absoluteHistory = [];
-  }
-
-  var restore = function() {
-    Lungo = _oldLungo;
-  }
-
-  var dom = function() {
-    return {
-      removeClass: noop
-      , attr: noop
-    };
-  }
-
-  var noop = function() {};
-
-  return {
-    Router: {
-      section: section
-      , article: article
-      , back: back
-      , history: {
-        get: getHistory
-        , clear: clearHistory
-      }
-    }
-    , View: {
-      Aside: {
-        toggle: noop
-      }
-    }
-    , init: noop
-    , restore: restore
-    , dom: dom
-  };
-};
-
-Lungo = Lungo.mock();
-
 describe('directives', function() {
 	beforeEach(function() {
 		angular.mock.module('Centralway.lungo-angular-bridge');
@@ -69,11 +8,11 @@ describe('directives', function() {
 
 	describe('lab-boot', function() {
 		it('should call Lungo init', function() {
-      inject(function($compile, $rootScope) {
-        var element = $compile('<div lab-boot></div>')($rootScope);
-        expect(Lungo.init).toHaveBeenCalled();
-        expect(Lungo.init.mostRecentCall.args[0]).toEqual({});
-      });
+          inject(function($compile, $rootScope) {
+            var element = $compile('<div lab-boot></div>')($rootScope);
+            expect(Lungo.init).toHaveBeenCalled();
+            expect(Lungo.init.mostRecentCall.args[0]).toEqual({});
+          });
 		});
 
 		it('should instantiate the AppRouter', function() {
