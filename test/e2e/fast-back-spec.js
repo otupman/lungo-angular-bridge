8e8a34f0632009d70beac09d1e14efb4ddc8069a
Tiago Duarte <tmpduarte@gmail.com>
2013-04-24 17:04:34 +0200

    issue-52 - problem found, e2e test writen hiiiijaaaa

---
+++ test/e2e/fast-back-spec.js
@@ -0,0 +1,29 @@
+describe('Fast back', function() {
+
+  beforeEach(function() {
+    browser().navigateTo('../test/cases/fast-back-test.html#/screen1');
+  });
+
+
+  ddescribe('Going back 3 times realy fast', function() {
+    beforeEach(function() {
+      element('#go-to-screen-2').click();
+      element('#go-to-screen-3').click();
+      element('#go-to-screen-4').click();
+    });
+
+    it('should be on screen 3', function() {
+      //let's go back reeeealy fast
+      element('#screen4 nav.left a').click();
+      element('#screen3 nav.left a').click();
+      element('#screen2 nav.left a').click();
+
+      //opening pandora box (aside)
+      element('#screen1 nav.left a').click();
+      //there you go
+      pause();
+
+      expect(element('section.lab-old-view[data-transition="slide"]').count()).toBe(1);
+    });
+  });
+});
