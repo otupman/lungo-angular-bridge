bf2053a085656590c72edcd33d6a19b0d4faa0d2
Oliver Tupman <oliver.tupman@centralway.com>
2013-05-27 18:23:07 -0400

    improvement(asides) - swipe-to-open is now configurable on a per-aside basis or globally via a lab-boot option.

--- src/directives/labBoot.js
+++ src/directives/labBoot.js
@@ -3,10 +3,25 @@ angular.module('Centralway.lungo-angular-bridge')
       function _parseResourceParam(param) {
         return param.indexOf(',') === -1 ? param : param.split(',');
       }
+    
+      function _getOptionBoolean(attrs, optionName, defaultValue) {
+        if(!attrs[optionName]) {
+          return defaultValue;
+        }
+        if(attrs[optionName].length === 0) {
+          return true;
+        }
+        return attrs[optionName].toLowerCase() === 'true';
+      }
+    
       return function(scope, elm, attrs) {
         Lungo.init({
           'resources': elm.attr('resources') && _parseResourceParam(elm.attr('resources'))
         });
+        var labOptions = {
+          doAsideSwipe: _getOptionBoolean(attrs, 'swipeOnAsides', true)
+        };
+        angular.module('Centralway.lungo-angular-bridge').value('labOptions', labOptions);
         AppRouter.instance = AppRouter(Lungo, $location, scope);
       };
     }])
--- test/spec/directives.spec.js
+++ test/spec/directives.spec.js
@@ -67,63 +67,9 @@ describe('directives', function() {
     });
   });
   
-  describe('lab-aside', function() {
-    
-    beforeEach(function() {
-     
-      
-      inject(function($compile, $rootScope) {
-        var element = $compile(
-         '<aside id="testAside"></aside>'
-        +'<section><header>'
-        +'<nav><a lab-aside="testAside">Test aside</a></nav>'
-        +'</header><article class="active"></article></section>'
-        )($rootScope);
-      });
-    });
-    
-    it('should have registered the swiping handlers', function() {
-      expect(domResponse.swiping).toHaveBeenCalled();
-      expect(domResponse.swipe).toHaveBeenCalled();
-    });
-  });
   
-  describe('lab-boot', function() {
-    it('should call Lungo init', function() {
-      inject(function($compile, $rootScope) {
-        var element = $compile('<div lab-boot></div>')($rootScope);
-        expect(Lungo.init).toHaveBeenCalled();
-        expect(Lungo.init.mostRecentCall.args[0]).toEqual({});
-      });
-    });
-
-    it('should instantiate the AppRouter', function() {
-      AppRouter = jasmine.createSpy('AppRouter');
-      inject(function($compile, $rootScope, $location) {
-        var element = $compile('<div lab-boot resources="singleFile.html"></div>')($rootScope);
-        expect(AppRouter).toHaveBeenCalled();
-        expect(AppRouter).toHaveBeenCalledWith(Lungo, $location, $rootScope);
-      });
-
-    });
-
-    describe('with resources', function() {
-      it('should pass on a single file correctly', function() {
-        inject(function($compile, $rootScope) {
-          var element = $compile('<div lab-boot resources="singleFile.html"></div>')($rootScope);
-          expect(Lungo.init.mostRecentCall.args[0]).toEqual({resources: 'singleFile.html'});
-        });
-      });
-
-      it('should pass on a file list correctly', function() {
-        inject(function($compile, $rootScope) {
-          var element = $compile('<div lab-boot resources="first.html,second.html"></div>')($rootScope);
-          expect(Lungo.init.mostRecentCall.args[0]).toEqual({resources: ['first.html', 'second.html']});
-        });
-      });
-    });
   
-  });
+
 
 });
 
